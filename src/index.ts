97e07f3ecdbc5de8ae8c97e87b9e3ab191ff0fbe
Keisuke Nakayama <baskk4.dt@gmail.com>
2020-02-02 19:43:19 +0900

    Fix style

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import regExpString2NFA from "./regExpString2NFA";
 import validate from "./validate";
 
-
 const input:string = process.argv[2];
 
 if( typeof input !== 'string' || input.length === 0 || !validate(input)){
